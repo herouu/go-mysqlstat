f5eb5ebba2c5874c8a90111788d22fc1eb04ae0b
herouu <herouucn@foxmail.com>
2024-05-19 00:41:16 +0800

    feat: topFrequentlySql

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/aditya43/clear-shell-screen-golang v0.0.0-20200419021057-6fa7bb4a928b // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gookit/goutil v0.6.15 // indirect
 	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -25,4 +26,5 @@ require (
 	golang.org/x/exp v0.0.0-20221208152030-732eee02a75a // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/duke-git/lancet/v2/convertor"
 	list "github.com/duke-git/lancet/v2/datastructure/list"
 	"github.com/duke-git/lancet/v2/datetime"
+	"github.com/gookit/goutil/strutil"
+	"github.com/gookit/goutil/timex"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/urfave/cli/v2"
@@ -91,14 +94,62 @@ func main() {
 		},
 		&cli.StringFlag{
 			Name:     "top",
-			Value:    "",
 			Usage:    "需要提供一个整数类型的参数值，该参数值表示执行次数最频繁的前N条SQL语句",
 			Required: false,
-			Action:   topFrequentlySql,
+		},
+		&cli.StringFlag{
+			Name:     "io",
+			Usage:    "需要提供一个整数类型的参数值，该参数值表示访问次数最频繁的前N张表文件ibd",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "lock",
+			Usage:    "查看当前锁阻塞的SQL",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "kill",
+			Usage:    "杀死当前锁阻塞的SQL",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "index",
+			Usage:    "查看重复或冗余的索引",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "conn",
+			Usage:    "查看应用端IP连接数总和",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "tinfo",
+			Usage:    "统计库里每个表的大小",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "fpk",
+			Usage:    "快速找出没有主键的表",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "dead",
+			Usage:    "查看死锁信息",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "binlog",
+			Usage:    "Binlog分析-高峰期排查哪些表TPS比较高",
+			Required: false,
+		},
+		&cli.BoolFlag{
+			Name:     "repl",
+			Usage:    "查看主从复制信息",
+			Required: false,
 		},
 	}
 
-	app.Action = mysqlStatusMonitor
+	app.Action = ctrlAction
 	app.Version = "1.0.0"
 	err := app.Run(os.Args)
 	if err != nil {
@@ -106,9 +157,50 @@ func main() {
 	}
 }
 
-func mysqlStatusMonitor(c *cli.Context) error {
+func ctrlAction(context *cli.Context) error {
+
+	ip := context.String("mysql_ip")
+	pwd := context.String("mysql_password")
+	port := context.String("mysql_port")
+	name := context.String("mysql_user")
+
+	// 初始化数据库
+	dbInit(ip, pwd, port, name)
+
+	topV := context.String("top")
+	ioV := context.String("io")
+	lock := context.Bool("lock")
+	kill := context.Bool("kill")
+	index := context.Bool("index")
+	conn := context.Bool("conn")
+	tinfo := context.Bool("tinfo")
+
+	if strutil.IsNotBlank(topV) {
+		showFrequentlySql(topV)
+	} else if strutil.IsNotBlank(ioV) {
+		showFrequentlyIo(ioV)
+	} else if lock && kill {
+		showLockSql(true)
+	} else if lock {
+		showLockSql(false)
+	} else if kill {
+		fmt.Println("Error: --kill requires --lock")
+	} else if index {
+		showRedundantIndexes()
+	} else if conn {
+		showConnCount(ip, port)
+	} else if tinfo {
+		showTableInfo()
+	} else {
+		mysqlStatusMonitor()
+	}
+	return nil
+
+}
+
+func mysqlStatusMonitor() {
 	// 初始化数据库连接
-	dbInit(c)
+
 	sigs := make(chan os.Signal, 1)
 	//注册信号处理函数
 	// Ctrl+C Ctrl+Z
@@ -160,14 +252,10 @@ func mysqlStatusMonitor(c *cli.Context) error {
 		}
 	}()
 	<-sigs
-	return nil
 }
 
-func dbInit(c *cli.Context) {
-	ip := c.String("mysql_ip")
-	pwd := c.String("mysql_password")
-	port := c.String("mysql_port")
-	name := c.String("mysql_user")
+func dbInit(ip, pwd, port, name string) {
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/information_schema?charset=utf8mb4&parseTime=True&loc=Local", name, pwd, ip, port)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -190,15 +278,7 @@ type DbResult struct {
 // 指标计算
 func calQuota() *Quota {
 
-	var sc DbResult
-	var ic DbResult
-	var uc DbResult
-	var dc DbResult
-	var mc DbResult
-	var br DbResult
-	var bs DbResult
-	var tc DbResult
-
+	var sc, ic, uc, dc, mc, br, bs, tc DbResult
 	//获取数据库的初始统计信息
 	DB.Raw("SHOW GLOBAL STATUS LIKE 'Com_select'").Scan(&sc)
 	DB.Raw("SHOW GLOBAL STATUS LIKE 'Com_insert'").Scan(&ic)
@@ -264,26 +344,25 @@ func callClear() {
 	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
 	if ok {                          //if we defined a clear func for that platform:
 		value() //we execute it
-	} else { //unsupported platform
+	} else {
 		panic("Your platform is unsupported! I can't clear terminal screen :(")
 	}
 }
 
-func topFrequentlySql(c *cli.Context, top string) error {
-	// 初始化数据库连接
-	dbInit(c)
+func showFrequentlySql(top string) {
+
 	var isPerformanceSchema int
 	DB.Raw("SELECT @@performance_schema").Scan(&isPerformanceSchema)
 
 	if isPerformanceSchema == 0 {
 		fmt.Println("performance_schema参数未开启。")
 		fmt.Println("在my.cnf配置文件里添加performance_schema=1，并重启mysqld进程生效。")
-		os.Exit(0)
+		return
 	}
 
-	DB.Raw("SET @sys.statement_truncate_len = 1024")
-	rows, err := DB.Raw("select query,db,last_seen,exec_count,max_latency,avg_latency from sys.statement_analysis order by exec_count desc, "+
-		"last_seen desc limit @top", sql.Named("top", top)).Rows()
+	DB.Exec("SET @sys.statement_truncate_len = 1024")
+	rows, err :=
+		DB.Raw("select query,db,last_seen,exec_count,max_latency,avg_latency from sys.statement_analysis order by exec_count desc,last_seen desc limit @top", sql.Named("top", top)).Rows()
 	defer func(rows *sql.Rows) {
 		err := rows.Close()
 		if err != nil {
@@ -291,7 +370,7 @@ func topFrequentlySql(c *cli.Context, top string) error {
 		}
 	}(rows)
 	if err != nil {
-		return err
+		fmt.Println(err)
 	}
 	// 表格
 	tw := table.NewWriter()
@@ -314,8 +393,345 @@ func topFrequentlySql(c *cli.Context, top string) error {
 		if err != nil {
 			fmt.Println(err)
 		}
-		tw.AppendRow(table.Row{query.String, db.String, lastSeen.Time.Format("2006-01-02 15:04:05.000000"), execCount.String, maxLatency.String, avgLatency.String})
+		dateFormat := timex.New(lastSeen.Time).DateFormat(timex.TemplateWithMs3)
+		tw.AppendRow(table.Row{query.String, db.String, dateFormat, execCount.String, maxLatency.String, avgLatency.String})
 	}
 	fmt.Println(tw.Render())
-	return nil
+}
+
+func showFrequentlyIo(ioV string) {
+
+	var isPerformanceSchema int
+	DB.Raw("SELECT @@performance_schema").Scan(&isPerformanceSchema)
+
+	if isPerformanceSchema == 0 {
+		fmt.Println("performance_schema参数未开启。")
+		fmt.Println("在my.cnf配置文件里添加performance_schema=1，并重启mysqld进程生效。")
+		return
+	}
+
+	DB.Exec("SET @sys.statement_truncate_len = 1024")
+	rows, err :=
+		DB.Raw("select file,count_read,total_read,count_write,total_written,total from sys.io_global_by_file_by_bytes limit @io", sql.Named("io", ioV)).Rows()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 表格
+	tw := table.NewWriter()
+	tw.SetTitle("Query Analysis")
+	tw.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	tw.AppendHeader(table.Row{"表文件名", "总共读取次数", "总共读取数据量", "总共写入次数", "总共写入数据量", "总共读写数据量"})
+
+	for rows.Next() {
+		var file, countRead, totalRead, countWrite, totalWritten, total sql.NullString
+		err := rows.Scan(&file, &countRead, &totalRead, &countWrite, &totalWritten, &total)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tw.AppendRow(table.Row{file.String, countRead.String, totalRead.String, countWrite.String, totalWritten.String, total.String})
+	}
+	fmt.Println(tw.Render())
+}
+
+func showLockSql(kill bool) {
+
+	rows, err := DB.Raw(`SELECT 
+            a.trx_id AS trx_id, 
+            a.trx_state AS trx_state, 
+            a.trx_started AS trx_started, 
+            b.id AS processlist_id, 
+            b.info AS info, 
+            b.user AS user, 
+            b.host AS host, 
+            b.db AS db, 
+            b.command AS command, 
+            b.state AS state, 
+            CONCAT('KILL QUERY ', b.id) AS sql_kill_blocking_query
+        FROM 
+            information_schema.INNODB_TRX a, 
+            information_schema.PROCESSLIST b 
+        WHERE 
+            a.trx_mysql_thread_id = b.id
+        ORDER BY 
+            a.trx_started`).Rows()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 表格
+	tw := table.NewWriter()
+	tw.SetTitle("Lock Blocking")
+	tw.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	tw.AppendHeader(table.Row{"事务ID", "事务状态", "执行时间", "线程ID", "info", "user", "host", "db", "command", "state", "kill阻塞查询ID"})
+
+	for rows.Next() {
+		var trxId, trxState, trxStarted, processlistId, info, user, host, db, command, state, sqlKillBlockingQuery sql.NullString
+		err := rows.Scan(&trxId, &trxState, &trxStarted, &processlistId, &info, &user, &host, &db, &command, &state, &sqlKillBlockingQuery)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tw.AppendRow(table.Row{trxId.String, trxState.String, trxStarted.String, processlistId.String, info.String, user.String, host.String,
+			db.String, command.String, state.String, sqlKillBlockingQuery.String})
+	}
+	fmt.Println(tw.Render())
+
+	// kill掉被锁住的sql
+	if kill {
+		kRow, err := DB.Raw(`SELECT 
+                a.trx_id AS trx_id, 
+                a.trx_state AS trx_state, 
+                a.trx_started AS trx_started, 
+                b.id AS processlist_id, 
+                b.info AS info, 
+                b.user AS user, 
+                b.host AS host, 
+                b.db AS db, 
+                b.command AS command, 
+                b.state AS state, 
+                CONCAT('KILL CONNECTION ', b.id) AS sql_kill_blocking_query
+            FROM 
+                information_schema.INNODB_TRX a, 
+                information_schema.PROCESSLIST b 
+            WHERE 
+                a.trx_mysql_thread_id = b.id and a.trx_state = 'RUNNING'
+            ORDER BY 
+                a.trx_started`).Rows()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		newList := list.NewList([]string{})
+		defer func(kRow *sql.Rows) {
+			err := kRow.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}(kRow)
+		for kRow.Next() {
+			var trxId, trxState, trxStarted, processlistId, info, user, host, db, command, state, sqlKillBlockingQuery sql.NullString
+			err := kRow.Scan(&trxId, &trxState, &trxStarted, &processlistId, &info, &user, &host, &db, &command, &state, &sqlKillBlockingQuery)
+			if err != nil {
+				fmt.Println(err)
+			}
+			newList.Push(sqlKillBlockingQuery.String)
+		}
+		newList.ForEach(func(sqlKill string) {
+			DB.Exec(sqlKill)
+			fmt.Printf("已成功执行 %s\n", sqlKill)
+		})
+	}
+}
+
+func showRedundantIndexes() {
+
+	var isPerformanceSchema int
+	DB.Raw("SELECT @@performance_schema").Scan(&isPerformanceSchema)
+
+	if isPerformanceSchema == 0 {
+		fmt.Println("performance_schema参数未开启。")
+		fmt.Println("在my.cnf配置文件里添加performance_schema=1，并重启mysqld进程生效。")
+		return
+	}
+
+	DB.Exec("SET @sys.statement_truncate_len = 1024")
+	rows, err :=
+		DB.Raw(`select table_schema,table_name,redundant_index_name,redundant_index_columns,
+       sql_drop_index from sys.schema_redundant_indexes`).Rows()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 表格
+	tw := table.NewWriter()
+	tw.SetTitle("Index Analysis")
+	tw.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	tw.AppendHeader(table.Row{"数据库名", "表名", "冗余索引名", "冗余索引列名", "删除冗余索引SQL"})
+
+	for rows.Next() {
+		var tableSchema, tableName, redundantIndexName, redundantIndexColumns, sqlDropIndex sql.NullString
+		err := rows.Scan(&tableSchema, &tableName, &redundantIndexName, &redundantIndexColumns, &sqlDropIndex)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tw.AppendRow(table.Row{tableSchema.String, tableName.String, redundantIndexName.String, redundantIndexColumns.String, sqlDropIndex.String})
+	}
+	fmt.Println(tw.Render())
+}
+
+func showConnCount(ip, port string) {
+
+	rows, err :=
+		DB.Raw(`SELECT USER, COUNT(*) FROM information_schema.PROCESSLIST 
+                      GROUP BY USER ORDER BY COUNT(*) DESC`).Rows()
+
+	defer func(rows2 *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 表格IP total number of connections
+	tw := table.NewWriter()
+	tw.SetTitle("total number of connections")
+	tw.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	tw.AppendHeader(table.Row{"连接用户", "数量"})
+
+	cc := int64(0)
+	for rows.Next() {
+		var user, count sql.NullString
+		err := rows.Scan(&user, &count)
+		if err != nil {
+			fmt.Println(err)
+		}
+		toInt, _ := convertor.ToInt(count.String)
+		cc += toInt
+		tw.AppendRow(table.Row{user.String, count.String})
+	}
+	fmt.Println("1) 连接数总和")
+	fmt.Println(tw.Render())
+	fmt.Printf("%s:%s 这台数据库，总共有 【%d】 个连接数\n", ip, port, cc)
+
+	// IP addresses
+	rowsIp, err := DB.Raw(`SELECT user,db,substring_index(HOST,':',1) AS Client_IP,count(1) AS count
+					FROM information_schema.PROCESSLIST 
+					GROUP BY user,db,substring_index(HOST,':',1) 
+					ORDER BY COUNT(1) DESC`).Rows()
+	defer func(rowsIp *sql.Rows) {
+		err := rowsIp.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rowsIp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 表格IP addresses
+	twIp := table.NewWriter()
+	twIp.SetTitle("Total number of connections from application-side IP addresses")
+	twIp.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	twIp.AppendHeader(table.Row{"连接用户", "数据库名", "应用端IP", "数量"})
+
+	for rowsIp.Next() {
+		var user, db, clientIp, count sql.NullString
+		err := rowsIp.Scan(&user, &db, &clientIp, &count)
+		if err != nil {
+			fmt.Println(err)
+		}
+		twIp.AppendRow(table.Row{user.String, db.String, clientIp.String, count.String})
+	}
+
+	fmt.Println()
+	fmt.Println("2) 应用端IP连接数总和")
+	fmt.Println(twIp.Render())
+
+}
+
+func showTableInfo() {
+
+	DB.Exec("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))")
+	rows, err :=
+		DB.Raw(` SELECT t.TABLE_SCHEMA as TABLE_SCHEMA, t.TABLE_NAME as TABLE_NAME, t.ENGINE as ENGINE,
+            IFNULL(t.DATA_LENGTH/1024/1024/1024, 0) as DATA_LENGTH,
+            IFNULL(t.INDEX_LENGTH/1024/1024/1024, 0) as INDEX_LENGTH,
+            IFNULL((DATA_LENGTH+INDEX_LENGTH)/1024/1024/1024, 0) AS TOTAL_LENGTH,
+            c.column_name AS COLUMN_NAME, c.data_type AS DATA_TYPE, c.COLUMN_TYPE AS COLUMN_TYPE,
+            t.AUTO_INCREMENT AS AUTO_INCREMENT, locate('unsigned', c.COLUMN_TYPE) = 0 AS IS_SIGNED 
+        FROM information_schema.TABLES t 
+        JOIN information_schema.COLUMNS c ON t.TABLE_SCHEMA = c.TABLE_SCHEMA AND t.table_name=c.table_name 
+        WHERE t.TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys') 
+        GROUP BY TABLE_NAME 
+        ORDER BY TOTAL_LENGTH DESC, AUTO_INCREMENT DESC`).Rows()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 表格
+	tw := table.NewWriter()
+	tw.SetTitle("Table Size Statistics")
+	tw.SetStyle(table.Style{
+		Name:    "StyleDefault",
+		Box:     table.StyleBoxDefault,
+		Color:   table.ColorOptionsDefault,
+		Format:  table.FormatOptionsDefault,
+		HTML:    table.DefaultHTMLOptions,
+		Options: table.OptionsDefault,
+		Title:   table.TitleOptions{Align: text.AlignCenter},
+	})
+	tw.AppendHeader(table.Row{"库名", "表名", "存储引擎", "数据大小(GB)", "索引大小(GB)", "总计(GB)", "主键字段名", "主键字段属性", "主键自增当前值", "主键自增值剩余"})
+
+	for rows.Next() {
+		var tableSchema, tableName, engine, dataLength, indexLength, totalLength, columnName, dataType, columnType, autoIncrement, isSigned sql.NullString
+		err := rows.Scan(&tableSchema, &tableName, &engine, &dataLength, &indexLength, &totalLength, &columnName, &dataType, &columnType, &autoIncrement, &isSigned)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// todo 待定
+		residualAutoIncrement := ""
+		if strutil.IsBlank(autoIncrement.String) {
+			residualAutoIncrement = "主键非自增"
+		}
+		tw.AppendRow(table.Row{tableSchema.String, tableName.String, engine.String, dataLength.String, indexLength.String, totalLength.String, columnName.String, columnType.String, autoIncrement.String, residualAutoIncrement})
+	}
+	fmt.Println(tw.Render())
 }
